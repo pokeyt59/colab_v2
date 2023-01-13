abd42f15c698f9252d430c064444d539c9aa74d2
pokety59 <cuphe@tobiasComputer.Jesus>
2023-01-13 07:21:35 -0600

    --------pt2 for git hub------------------------------------
    11. added boss named the creator it is a warden-ish style boss best to avoid untill late game
    
    12. updated boss type to undead
    
    13. added projectile launcher from rafradek_tf2_weapons-1.12.2-1.6.1 sence the mod seems to have been abantoned and i am a fan of team fortress 2 my self
    
    14. added syrenge

--- src/main/java/pokey/alexs/mod/entity/CrusaderscrossbowEntity.java
+++ src/main/java/pokey/alexs/mod/entity/CrusaderscrossbowEntity.java
@@ -1,6 +1,7 @@
 
 package pokey.alexs.mod.entity;
 
+import pokey.alexs.mod.procedures.HealorramdomeffectProcedure;
 import pokey.alexs.mod.init.PokeyAndAlexsModModEntities;
 
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,10 +10,12 @@ import net.minecraftforge.network.NetworkHooks;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.projectile.ItemSupplier;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.sounds.SoundSource;
@@ -59,6 +62,18 @@ public class CrusaderscrossbowEntity extends AbstractArrow implements ItemSuppli
 	protected void doPostHurtEffects(LivingEntity entity) {
 		super.doPostHurtEffects(entity);
 		entity.setArrowCount(entity.getArrowCount() - 1);
+	}
+
+	@Override
+	public void playerTouch(Player entity) {
+		super.playerTouch(entity);
+		HealorramdomeffectProcedure.execute(entity);
+	}
+
+	@Override
+	public void onHitEntity(EntityHitResult entityHitResult) {
+		super.onHitEntity(entityHitResult);
+		HealorramdomeffectProcedure.execute(entityHitResult.getEntity());
 	}
 
 	@Override
